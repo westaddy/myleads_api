9021b47e49afa6216e1444e8e379cfddc32931ed
Addy Salam <westaddy2000@gmail.com>
2019-08-12 18:15:38 +0200

    fixed bug

--- app/Services/LeadImpl.php
+++ app/Services/LeadImpl.php
@@ -56,6 +56,6 @@ class LeadImpl implements LeadContract {
 
     public function allByCreatedAt($created_at)
     {
-        return $this->lead->where('created_at',$created_at)->get();
+        return $this->lead->whereDate('created_at',$created_at)->get();
     }
 }
